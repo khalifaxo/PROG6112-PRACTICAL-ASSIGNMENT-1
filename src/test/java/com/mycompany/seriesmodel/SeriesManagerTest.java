0bae82ca3615b8e436605522838b67c91fbf2224
khalifaxo <riyaadkhalifa59@gmail.com>
2025-09-08 11:15:24 +0200

    Updated SeriesManagerTest.java

--- src/test/java/com/mycompany/seriesmodel/SeriesManagerTest.java
+++ src/test/java/com/mycompany/seriesmodel/SeriesManagerTest.java
@@ -1,155 +1,85 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
- */
 package com.mycompany.seriesmodel;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
- *
- * @author RC_Student_lab
+ * Unit tests for SeriesManager
  */
 public class SeriesManagerTest {
-    
-    public SeriesManagerTest() {
-    }
-    
+
     @BeforeClass
     public static void setUpClass() {
+        // preload some series before tests
+        SeriesManager.seriesList.clear();
+        SeriesManager.seriesList.add(new SeriesModel("S1", "Breaking Bad", 18));
+        SeriesManager.seriesList.add(new SeriesModel("S2", "Stranger Things", 16));
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
+        SeriesManager.seriesList.clear();
     }
 
     @Test
-    public void testMain() {
-        System.out.println("main");
-        String[] args = null;
-        SeriesManager.main(args);
-        fail("The test case is a prototype.");
+    public void testSearchSeriesById_found() {
+        SeriesModel result = SeriesManager.searchSeriesById("S1");
+        assertNotNull("Series should be found", result);
+        assertEquals("Breaking Bad", result.SeriesName);
     }
 
     @Test
-    public void testShowMenu() {
-        System.out.println("showMenu");
-        SeriesManager.showMenu();
-        fail("The test case is a prototype.");
+    public void testSearchSeriesById_notFound() {
+        SeriesModel result = SeriesManager.searchSeriesById("S99");
+        assertNull("Series should not be found", result);
     }
 
     @Test
-    public void testCaptureSeries() {
-        System.out.println("captureSeries");
-        SeriesManager.captureSeries();
-        fail("The test case is a prototype.");
+    public void testUpdateSeriesAge_success() {
+        boolean updated = SeriesManager.updateSeriesAge("S2", 13);
+        assertTrue("Age restriction should be updated", updated);
+
+        SeriesModel updatedSeries = SeriesManager.searchSeriesById("S2");
+        assertEquals(13, updatedSeries.AgeRestriction);
     }
 
     @Test
-    public void testSearchSeries() {
-        System.out.println("searchSeries");
-        SeriesManager.searchSeries();
-        fail("The test case is a prototype.");
+    public void testUpdateSeriesAge_fail() {
+        boolean updated = SeriesManager.updateSeriesAge("X999", 12);
+        assertFalse("Update should fail for non-existent ID", updated);
     }
 
     @Test
-    public void testUpdateAgeRestriction() {
-        System.out.println("updateAgeRestriction");
-        SeriesManager.updateAgeRestriction();
-        fail("The test case is a prototype.");
+    public void testDeleteSeriesById_success() {
+        boolean deleted = SeriesManager.deleteSeriesById("S1");
+        assertTrue("Series should be deleted", deleted);
+        assertNull("Deleted series should no longer exist",
+                   SeriesManager.searchSeriesById("S1"));
     }
 
     @Test
-    public void testDeleteSeries() {
-        System.out.println("deleteSeries");
-        SeriesManager.deleteSeries();
-        fail("The test case is a prototype.");
-    }
-
-    @Test
-    public void testPrintReport() {
-        System.out.println("printReport");
-        SeriesManager.printReport();
-        fail("The test case is a prototype.");
-    }
-
-    @Test
-    public void testSearchSeriesById() {
-        System.out.println("searchSeriesById");
-        String id = "";
-        SeriesModel expResult = null;
-        SeriesModel result = SeriesManager.searchSeriesById(id);
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
-    }
-
-    @Test
-    public void testUpdateSeriesAge() {
-        System.out.println("updateSeriesAge");
-        String id = "";
-        int newAge = 0;
-        boolean expResult = false;
-        boolean result = SeriesManager.updateSeriesAge(id, newAge);
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
-    }
-
-    @Test
-    public void testDeleteSeriesById() {
-        System.out.println("deleteSeriesById");
-        String id = "";
-        boolean expResult = false;
-        boolean result = SeriesManager.deleteSeriesById(id);
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
+    public void testDeleteSeriesById_fail() {
+        boolean deleted = SeriesManager.deleteSeriesById("X000");
+        assertFalse("Delete should fail for non-existent ID", deleted);
     }
 
     @Test
     public void testIsValidAgeRestriction() {
-        System.out.println("isValidAgeRestriction");
-        int age = 0;
-        boolean expResult = false;
-        boolean result = SeriesManager.isValidAgeRestriction(age);
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
-    }
-
-    @Test
-    public void testPreloadSeries() {
-        System.out.println("preloadSeries");
-        SeriesManager.preloadSeries();
-        fail("The test case is a prototype.");
-    }
-
-    @Test
-    public void testUpdateSeries() {
-        System.out.println("updateSeries");
-        SeriesManager.updateSeries();
-        fail("The test case is a prototype.");
-    }
-
-    @Test
-    public void testSearchByName() {
-        System.out.println("searchByName");
-        SeriesManager.searchByName();
-        fail("The test case is a prototype.");
-    }
-
-    @Test
-    public void testListByAgeRestriction() {
-        System.out.println("listByAgeRestriction");
-        SeriesManager.listByAgeRestriction();
-        fail("The test case is a prototype.");
+        assertTrue(SeriesManager.isValidAgeRestriction(13));
+        assertTrue(SeriesManager.isValidAgeRestriction(18));
+        assertFalse(SeriesManager.isValidAgeRestriction(-1));
+        assertFalse(SeriesManager.isValidAgeRestriction(200));
     }
 
     @Test
     public void testShowTotalSeries() {
-        System.out.println("showTotalSeries");
-        SeriesManager.showTotalSeries();
-        fail("The test case is a prototype.");
+        int before = SeriesManager.seriesList.size();
+        SeriesManager.seriesList.add(new SeriesModel("S3", "The Witcher", 18));
+        int after = SeriesManager.seriesList.size();
+        assertEquals(before + 1, after);
     }
-    
 }
+
